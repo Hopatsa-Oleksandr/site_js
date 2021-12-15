8e6919b0472c094586a17db2c9e19cb52b1e05e1
Hopatsa-Oleksandr <bc.alexg@gmail.com>
2021-12-15 23:47:32 +0200

    добавляет файлы node_js

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ const model = [
   { type: "text", value: "here we go with some text" },
   {
     type: "columns",
-    value: ["11111111111", "22222222222", "33333333333"],
+    value: [
+      "11111111111",
+      "22222222222",
+      "33333333333",
+      "444444444",
+      "555555555",
+      "66666666",
+    ],
   },
+  { type: "image", value: "./assets/image.png" },
 ];
 
 const $site = document.querySelector("#site"); //DOM елемент
@@ -13,25 +21,11 @@ model.forEach((block) => {
   let html = "";
   //   console.log(block);
   if (block.type === "title") {
-    html = `
-      <div class="row">
-          <div class="col-sm">
-            <h1>${block.value}</h1>
-          </div>
-        </div>
-    `;
+    html = title(block);
   } else if (block.type === "text") {
-    html = `
-       <div class="row">
-          <div class="col-sm">
-            <p>
-              ${block.value}
-            </p>
-          </div>
-        </div>
-      `;
+    html = text(block);
   } else if (block.type === "columns") {
-    // html = `
+    html = columns(block);
     //    <div class="row">
     //       <div class="col-sm">
     //         {block.value}
@@ -44,7 +38,57 @@ model.forEach((block) => {
     //       </div>
     //     </div>
     //   `;
+  } else if (block.type === "image") {
+    html = image(block);
   }
 
   $site.insertAdjacentHTML("beforeend", html);
 });
+
+function title(block) {
+  return `
+      <div class="row">
+          <div class="col-sm">
+            <h1>${block.value}</h1>
+          </div>
+        </div>
+    `;
+}
+
+function text(block) {
+  return `
+       <div class="row">
+          <div class="col-sm">
+            <p>
+              ${block.value}
+            </p>
+          </div>
+        </div>
+      `;
+}
+
+function columns(block) {
+  // let html = "";
+  // block.value.forEach((item) => {
+  //   html += `
+  //   <div class="col-sm">
+  //     ${item}
+  //   </div>`;
+  // });
+  const html = block.value.map(
+    (item) => `<div class="col-sm">
+      ${item}
+    </div>`
+  );
+  return `<div class="row">
+    ${html.join("")}
+  </div>`;
+}
+
+function image(block) {
+  return `
+  <div class="row">
+  <img src="${block.value}" />
+  </div>
+  `;
+}
